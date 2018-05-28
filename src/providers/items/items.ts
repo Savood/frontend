baba69b3529cebb86c0325e586682ed5e9388c40
TimMaa <tim.maasen@ymail.com>
2018-05-28 20:22:48 +0200

    Got trolled by ionic, didnt show errors but it had some

--- src/providers/items/items.ts
+++ src/providers/items/items.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 
 import { Item } from '../../models/item';
-// import { Api } from '../api/api';
+import { Api } from '../api/api';
 
 @Injectable()
 export class Items {
 
   constructor(
-    // public api: Api
+    public api: Api
   ) { }
 
   query(params?: any) {
-    // return this.api.get('/items', params);
+    return this.api.get('/items', params);
   }
 
   add(item: Item) {
