bcc44ee75a2c46911444bf8f6019dfa5592053f1
TimMaa <tim.maasen@ymail.com>
2018-05-31 15:16:50 +0200

    Make a better chat page (still not great)

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {MessagesService} from "../../providers";
 
 /**
  * Generated class for the ChatPage page.
@@ -15,20 +16,37 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 })
 export class ChatPage {
 
+  chatId: string;
+  newMessage: string;
+
   messages: any = [
     {
+      from: {
+        userId: "2",
+        firstname: "John",
+        lastname: "Johnson",
+        avatarId: "assets/img/speakers/bear.jpg"
+      },
       content: "Hallo 1",
-      sender: "2",
       timestamp: "1985-04-12T23:20:50.52Z"
     },
     {
       content: "Hallo 2",
-      sender: "1",
-      timestamp: "1985-04-12T23:20:50.52Z"
+      from: {
+        userId: "5",
+        firstname: "Bert",
+        lastname: "Likerson",
+        avatarId: "assets/img/speakers/iguana.jpg"
+      },
+      timestamp: "1985-04-12T23:23:50.52Z"
     }
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public _message: MessagesService) {
+    this.chatId = this.navParams.get("chatId")
   }
 
   ionViewDidLoad() {
@@ -36,6 +54,6 @@ export class ChatPage {
   }
 
   sendMessage(){
-
+    // this._message.createNewMessage(this.chatId,this.newMessage);
   }
 }
